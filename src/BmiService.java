1a28f0c7237ab4ef5fc0015d6a72f34f15e3117d
Pavel Osharin <mr.osharin@mail.ru>
2023-09-06 16:21:56 +0300

    Reformat cod

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate (int heightInKg, double weightInM) {
-        int result = (int)(heightInKg / (weightInM * weightInM));
+    public int calculate(int heightInKg, double weightInM) {
+        int result = (int) (heightInKg / (weightInM * weightInM));
         return result;
     }
 }
